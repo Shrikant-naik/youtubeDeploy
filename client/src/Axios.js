d2137828aa3a0e06caa59c044c030b24222fbb79
Shrikant-naik <nshrikant31@gmail.com>
2023-09-30 19:59:57 +0530

    no, this is finalcommit

--- client/src/Axios.js
+++ client/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL:"https://backendyoutube.onrender.com/",
+    baseURL:"https://backendyoutube.onrender.com/api/",
     withCredentials:true
 });
